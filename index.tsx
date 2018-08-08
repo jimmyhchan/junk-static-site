183f340df64443b9e0e40e8136b43c489032cfed
Jimmy Chan <jchan@linkedin.com>
2018-08-07 21:06:46 -0700

    react tutorial start

--- index.tsx
+++ index.tsx
@@ -1,7 +1,12 @@
 import './index.scss';
 import * as React from 'react';
 import {render} from 'react-dom';
-import {HelloMessage} from './src/components/hello';
+import { Game } from './src/components/game';
+// import {HelloMessage} from './src/components/hello';
 
 const mount = document.getElementById('app');
-render(<HelloMessage name="foo" />, mount);
+// render(<HelloMessage name="foo" />, mount);
+render(
+  <Game />, 
+  mount
+)
