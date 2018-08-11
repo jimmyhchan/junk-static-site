6ab2fd8b0df0d0807aa2058fe46c5d9315a69fec
Jimmy Chan <jchan@linkedin.com>
2018-08-10 21:42:16 -0700

    working game

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -13,6 +13,26 @@ type BoardState = {
 const getMark = (isNext:boolean):SqOption => {
   return isNext ? 'X' : 'O';
 }
+const calculateWinner = (squares: SqOption[]):SqOption => {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  for (let index = 0; index < lines.length; index++) {
+    const [a,b,c] = lines[index];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
 
 export class Board extends React.Component<object, BoardState> {
   readonly state:BoardState = initialBoardState;
@@ -26,7 +46,13 @@ export class Board extends React.Component<object, BoardState> {
 
   }
   render() {
-    const status = 'Next player: ' + (getMark(this.state.xIsNext));
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = 'Next player: ' + (getMark(this.state.xIsNext));
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -51,6 +77,9 @@ export class Board extends React.Component<object, BoardState> {
 
   private handleClick = (idx: number) => {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[idx]) {
+      return;
+    }
     squares[idx] = getMark(this.state.xIsNext);
     this.setState({
       squares,
