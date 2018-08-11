30e48009c15581e7b72f7a55eb66f73990f5a15e
Jimmy Chan <jchan@linkedin.com>
2018-08-10 20:41:54 -0700

    interactive

--- src/components/square.tsx
+++ src/components/square.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 
-export class Square extends React.Component {
+type SqOption = null | 'X' | 'O';
+
+const initialState = {
+  value: null
+}
+
+
+type SqState = {
+  value: SqOption
+}
+
+type SqProp = {
+  value: SqOption
+}
+export class Square extends React.Component <SqProp, SqState> {
+  readonly state:SqState = initialState;
+  handleClick = () => this.setState({ value: 'X'});
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={this.handleClick}>
+        {this.state.value}
       </button>
     )
   }
